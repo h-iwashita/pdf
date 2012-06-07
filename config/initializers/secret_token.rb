497c4e57510946b692845be29709843f166fef68
h-iwashita <nagi83282@gmail.com>
2012-06-07 18:22:54 +0900

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pdf::Application.config.secret_token = '1184183dc79d3fa9bfea7c6ec9614ce47d24f3240aaf53d1ebc41e2237dd6b1bcb3452d93cb3928a08e854c727a2f956a9f678a01647c0b0799f3f2913333e98'
