50748aebc0498f1a442b221378fa22271ba158f8
h-iwashita <nagi83282@gmail.com>
2012-06-08 13:20:55 +0900

    PDF出力テスト改

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,12 +1,15 @@
+# coding: utf-8
 class TestController < ApplicationController
   def render_reports
-    report = ThinReports::Report.create :layout => File.join(Rails.root, 'reports', 'test_01.tlf') do
-      
+    report = ThinReports::Report.new :layout => 'reports/test_01'
+    report.start_new_page do |page|
+      page.item(:year).value("平成24")
+      page.item(:address).value("埼玉県川口市赤井1−2−20 レオパレスTiara 205号室ああああああああああああああああああ")
+      page.item(:name).value("岩下　人美")
     end
-    report.generate_file("foo.pdf")
 
-#    send_data report.generate, :filename    => 'foo.pdf', 
-#                               :type        => 'application/pdf', 
-#                               :disposition => 'attachment'
+    send_data report.generate, :filename    => 'foo.pdf', 
+                               :type        => 'application/pdf', 
+                               :disposition => 'attachment'
   end
 end
